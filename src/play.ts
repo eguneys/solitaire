0aeae92e00ed4711809c7175fedd8a5a288d653e
Emre Guneyler <eguneys@gmail.com>
2021-10-21 21:03:52 +0000

    lkasdj"

--- src/play.ts
+++ src/play.ts
@@ -1,3 +1,22 @@
+import { Context } from './types'
 
 
+export default class Play {
+
+
+  constructor(readonly ctx: Context) {
+  }
+
+
+  init(x: number, y: number) {
+  }
+
+
+  update(dt: number) {
+  }
+
+
+  draw() {
+  }
+}
 
